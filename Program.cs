2dd366c5cc830d8c5fbbe36eeaad407555e885ee
Egois1 <148081215+Egois1@users.noreply.github.com>
2024-04-12 17:14:06 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types.Enums;
+using Telegram.Bot.Types;
+using Microsoft.VisualBasic;
+
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var botClient = new TelegramBotClient("6932018755:AAGdjlI43JCETcm_HBnLkK_OMgyXXNzs5tM");
+
+        var me = await botClient.GetMeAsync();
+        Console.WriteLine($"Hello, World! I am user {me.Id} and my name is {me.FirstName}.");
+
+        using CancellationTokenSource cts = new();
+
+        ReceiverOptions receiverOptions = new()
+        {
+            AllowedUpdates = Array.Empty<UpdateType>()
+        };
+
+        botClient.StartReceiving(
+            updateHandler: HandleUpdateAsync,
+            pollingErrorHandler: HandlePollingErrorAsync,
+            receiverOptions: receiverOptions,
+            cancellationToken: cts.Token
+        );
+
+        Console.ReadLine();
+        cts.Cancel();
+
+        async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+        {
+            var message = update.Message;
+            var input = message.Text;
+            if (!int.TryParse(input, out int i) || i < 1 || i > 1000)
+            {
+                await botClient.SendTextMessageAsync
+                (chatId: message.Chat.Id, text: "Выберите число от 1 до 1000.");
+                return;
+            }
+            var url = $"https://cataas.com/cat/says/Изображение №{i}";
+            using var httpClient = new HttpClient();
+            var response = await httpClient.GetAsync(url);
+
+            if (!response.IsSuccessStatusCode)
+                return;
+            Message sentMessage = await botClient.SendPhotoAsync
+            (chatId: message.Chat.Id,
+            photo: InputFile.FromUri(url),
+            cancellationToken: cancellationToken);
+        }
+
+        Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+        {
+            var ErrorMessage = exception switch
+            {
+                ApiRequestException apiRequestException => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+                _ => exception.ToString()
+            };
+
+            Console.WriteLine(ErrorMessage);
+            return Task.CompletedTask;
+        }
+    }
+}
